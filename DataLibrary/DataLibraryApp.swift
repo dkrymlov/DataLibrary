6f342c851bdf4561637da7790f6b9ccdfa02088c
dkrymlov <91798748+dkrymlov@users.noreply.github.com>
2023-09-28 15:17:52 +0300

    Initial Commit

---
+++ DataLibrary/DataLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataLibraryApp.swift
+//  DataLibrary
+//
+//  Created by Даниил Крымлов on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DataLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
